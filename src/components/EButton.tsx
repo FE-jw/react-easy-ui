f4511c26a1ce79cb85edaa4399ad07f7311869df
JW <zaixu91@gmail.com>
2026-01-19 14:40:03 +0900

    [feat] EButton 컴포넌트 작업
    - isDisabled props 추가
    - onClick이 전달된 경우에만 클릭 이벤트 등록

--- src/components/EButton.tsx
+++ src/components/EButton.tsx
@@ -5,14 +5,22 @@ export interface EButtonProps {
   children: React.ReactNode;
   className?: string;
   variant?: 'primary' | 'secondary';
+  isDisabled?: boolean;
   onClick?: () => void;
 }
 
-export function EButton({ children, className, variant = 'primary', onClick }: EButtonProps) {
-  const cn = `${className ? `${className} ` : ''}${style.button} ${style[variant]}`;
+export function EButton({ children, className, variant = 'primary', isDisabled = false, onClick }: EButtonProps) {
+  const cn = `${className ? `${className} ` : ''}${isDisabled ? `${style.isDisabled} ` : `${style[variant]} `}${style.button}`;
+
+  // onClick Handler
+  const handleOnClick = () => {
+    if (!onClick) return;
+
+    onClick();
+  };
 
   return (
-    <button type="button" className={cn} onClick={onClick}>
+    <button type="button" className={cn} onClick={handleOnClick}>
       {children}
     </button>
   );
