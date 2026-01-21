d107350fbcf3b5e3da21d7d4257a1cb4b994b5f5
FE-jw <FE-jw@users.noreply.github.com>
2026-01-21 09:15:36 +0000

    deploy: 4e1b6e4146de0cf27764f858d66ef103f4492111

--- storybook/components/EButton.stories.d.ts
+++ storybook/components/EButton.stories.d.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
+import { Meta, StoryObj } from '@storybook/react';
 import { EButton } from './EButton';
 
 declare const meta: Meta<typeof EButton>;
--- storybook/components/EInput.stories.d.ts
+++ storybook/components/EInput.stories.d.ts
@@ -1,4 +1,4 @@
-import { Meta, StoryObj } from '@storybook/react-vite';
+import { Meta, StoryObj } from '@storybook/react';
 import { EInput } from './EInput';
 
 declare const meta: Meta<typeof EInput>;
